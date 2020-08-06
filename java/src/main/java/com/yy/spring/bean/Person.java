88a57ccaf745f848f7f763dc06362feb15e5eba8
yu2yu <youny1118@gmail.com>
2020-08-06 16:55:17 +0800

    添加注释

--- java/src/main/java/com/yy/spring/bean/Person.java
+++ java/src/main/java/com/yy/spring/bean/Person.java
@@ -9,6 +9,20 @@ import org.springframework.beans.factory.InitializingBean;
 
 /**
  * @description: spring Bean的生命周期
+ *
+ * 1、Bean自身的方法:　　
+ *              这个包括了Bean本身调用的方法和通过配置文件中<bean>的init-method和destroy-method指定的方法
+ *
+ * 2、Bean级生命周期接口方法:　　
+ *              这个包括了BeanNameAware、BeanFactoryAware、InitializingBean和DiposableBean这些接口的方法
+ *
+ * 3、容器级生命周期接口方法:
+ *              这个包括了InstantiationAwareBeanPostProcessor 和 BeanPostProcessor 这两个接口实现，
+ *              一般称它们的实现类为“后处理器”。
+ *
+ * 4、工厂后处理器接口方法:
+ *              这个包括了AspectJWeavingEnabler, ConfigurationClassPostProcessor, CustomAutowireConfigurer等等
+ *              非常有用的工厂后处理器　　接口的方法。工厂后处理器也是容器级的。在应用上下文装配配置文件之后立即调用。
  * @author: yy
  * @date: 2020/8/6 16:03
  */
